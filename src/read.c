661d69036f8379b32e0ed68b485078035dce3d6d
deoxxa <deoxxa@fknsrs.biz>
2012-07-24 17:22:49 +1000

    make the mcnet_read_[u]int* functions platform-independent

--- src/read.c
+++ src/read.c
@@ -19,55 +19,23 @@ uint8_t mcnet_read_uint8(uint8_t* in) {
 }
 
 int16_t mcnet_read_int16(uint8_t* in) {
-  int16_t out;
-
-  uint8_t* t = (uint8_t*)&out;
-
-  t[0] = in[1];
-  t[1] = in[0];
-
-  return out;
+  return (in[0] << 8) + in[1];
 }
 
 uint16_t mcnet_read_uint16(uint8_t* in) {
-  uint16_t out;
-
-  uint8_t* t = (uint8_t*)&out;
-
-  t[0] = in[1];
-  t[1] = in[0];
-
-  return out;
+  return (in[0] << 8) + in[1];
 }
 
 int32_t mcnet_read_int32(uint8_t* in) {
-  int32_t out;
+  return (in[0] << 24) + (in[1] << 16) + (in[2] << 8) + in[3];
+}
 
-  uint8_t* t = (uint8_t*)&out;
-
-  t[0] = in[3];
-  t[1] = in[2];
-  t[2] = in[1];
-  t[3] = in[0];
-
-  return out;
+uint64_t mcnet_read_uint64(uint8_t* in) {
+  return (in[0] << 56) + (in[1] << 48) + (in[2] << 40) + (in[3] << 32) + (in[4] << 24) + (in[5] << 16) + (in[6] << 8) + in[7];
 }
 
 int64_t mcnet_read_int64(uint8_t* in) {
-  int64_t out;
-
-  uint8_t* t = (uint8_t*)&out;
-
-  t[0] = in[7];
-  t[1] = in[6];
-  t[2] = in[5];
-  t[3] = in[4];
-  t[4] = in[3];
-  t[5] = in[2];
-  t[6] = in[1];
-  t[7] = in[0];
-
-  return out;
+  return (in[0] << 56) + (in[1] << 48) + (in[2] << 40) + (in[3] << 32) + (in[4] << 24) + (in[5] << 16) + (in[6] << 8) + in[7];
 }
 
 float mcnet_read_float(uint8_t* in) {
